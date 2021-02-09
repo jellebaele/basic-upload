ab159fb3aa9e206bc8044111b438b9971197e919
Jelle Baele <jellebaele@hotmail.com>
2021-02-09 22:46:24 +0100

    basic project setup

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const routes = require("../routes/routes");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+app.use(routes);
+
+const port = process.env.PORT || "3000";
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`Server listening on http://localhost:${port}`);
+});
